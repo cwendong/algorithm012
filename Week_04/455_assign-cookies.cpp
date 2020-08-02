dbf0fbfc7c9baaf29ba9ec77bc7a8606e51b0d3b
cwendong <cwendong@gmail.com>
2020-08-02 17:34:47 +0800

    [add] leetcode problem 455: Assign cookies

---
+++ Week_04/455_assign-cookies.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int gIndex = 0;
+        int sIndex = 0;
+        while (gIndex < g.size() && sIndex < s.size())
+        {
+            if (g[gIndex] <= s[sIndex])
+            {
+                gIndex++;
+            }
+
+            sIndex++;
+        }
+
+        return gIndex;
+    }
+};
